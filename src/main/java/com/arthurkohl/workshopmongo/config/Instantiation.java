ecafe3711d5d7b01d8ce5a2d4b6f7382dd6c1dd1
Arthur Peruzzo <arthurkohl0@gmail.com>
2021-04-16 09:40:58 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/arthurkohl/workshopmongo/config/Instantiation.java
+++ src/main/java/com/arthurkohl/workshopmongo/config/Instantiation.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.arthurkohl.workshopmongo.domain.Post;
 import com.arthurkohl.workshopmongo.domain.User;
+import com.arthurkohl.workshopmongo.dto.AuthorDTO;
 import com.arthurkohl.workshopmongo.repository.PostRepository;
 import com.arthurkohl.workshopmongo.repository.UserRepository;
 
@@ -31,11 +32,12 @@ public class Instantiation implements CommandLineRunner{
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-
-		Post pos1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post pos2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-				
+		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post pos1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post pos2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+				
 		postRepository.saveAll(Arrays.asList(pos1, pos2));
 		
 	}
--- src/main/java/com/arthurkohl/workshopmongo/dto/AuthorDTO.java
+++ src/main/java/com/arthurkohl/workshopmongo/dto/AuthorDTO.java
@@ -5,22 +5,20 @@ import java.io.Serializable;
 import com.arthurkohl.workshopmongo.domain.User;
 
 /*
- Padrao DTO para filtrar somente atributos necessários para manipulação.
- */
-public class UserDTO implements Serializable{ 
+Padrao DTO para filtrar somente atributos necessários para manipulação.
+*/
+public class AuthorDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private String id;
 	private String name;
-	private String email;
 	
-	public UserDTO() {
+	public AuthorDTO() {
 	}
 	
-	public UserDTO(User obj) {
+	public AuthorDTO(User obj) {
 		id = obj.getId();
 		name = obj.getName();
-		email = obj.getEmail();
 	}
 
 	public String getId() {
@@ -38,14 +36,5 @@ public class UserDTO implements Serializable{
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
 	
 }
